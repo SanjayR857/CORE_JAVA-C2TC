9597f3f7b49d2feba8cfb7f730b1215b56f54199
SanjayR857 <sanjayr9586@gmail.com>
2022-06-14 21:29:55 +0530

    FIRST COMMIT

--- Day14/src/com/code3/Input.java
+++ Day14/src/com/code3/Input.java
@@ -1,5 +1,29 @@
 package com.code3;
 
-public class Input {
+import java.util.Scanner;
 
-}
+public class Input {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		System.out.println("output");
+		System.out.println(a+"\n"+b+"\n"+c);
+	}
+	}
+
+
+
+	/*
+	42
+	46
+	79
+	output
+	42
+	46
+	79
+	*/
+
+	
+
--- Day14/src/com/wrapper/INput.java
+++ Day14/src/com/wrapper/INput.java
@@ -1,5 +1,26 @@
 package com.wrapper;
 
+import java.util.Scanner;
+
 public class INput {
 
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		System.out.println("output");
+		System.out.println(a+"\n"+b+"\n"+c);
+	}
+	}
 }
+	/*
+	42
+	46
+	79
+	output
+	42
+	46
+	79
+	*/
+
